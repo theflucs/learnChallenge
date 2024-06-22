7171aa48f4e9308fc2fc5dd56b31e60f97174ec1
sabrina.cux <sa.cux.dev@gmail.com>
2024-06-22 17:05:36 +0200

    integrate rps logic in web ui

--- src/webAppScript.ts
+++ src/webAppScript.ts
@@ -1,106 +1,119 @@
 import {foobar} from './foobar/foobar.js';
+import {isValidMove, playGame} from './rps/rps.js';
+import type {GameMode, Move, Result} from './rps/types.js';
 
 const toggleFoobarButton = document.getElementById(
   'toggle-foobar-button',
-) as HTMLButtonElement | null;
+) as HTMLButtonElement;
 const toggleChooseModeButton = document.getElementById(
   'toggle-choose-mode-button',
-) as HTMLButtonElement | null;
+) as HTMLButtonElement;
 const foobarContainer = document.getElementById(
   'foobar-container',
-) as HTMLDivElement | null;
+) as HTMLDivElement;
 const gameModeContainer = document.getElementById(
   'game-mode-container',
-) as HTMLDivElement | null;
+) as HTMLDivElement;
 const gameDeck = document.getElementById('game-deck') as HTMLDivElement;
+
 const gameWrapper = document.getElementById('game-wrapper') as HTMLDivElement;
 const modeButtons = document.querySelectorAll(
   '.mode-button',
 ) as NodeListOf<HTMLButtonElement>;
+const moveButtons = document.querySelectorAll(
+  '.move-button',
+) as NodeListOf<HTMLButtonElement>;
+
+const player1Moves = document.getElementById('player1-moves') as HTMLDivElement;
+const player2Moves = document.getElementById('player2-moves') as HTMLDivElement;
+const player1Move = document.getElementById('player1-move') as HTMLSpanElement;
+const player2Move = document.getElementById('player2-move') as HTMLSpanElement;
+const gameResultElement = document.getElementById(
+  'game-result',
+) as HTMLHeadingElement;
+
+const computerPlaceholder = document.getElementById(
+  'computer-placeholder',
+) as HTMLDivElement;
+const computersPlaceholder = document.getElementById(
+  'computers-placeholder',
+) as HTMLDivElement;
+
+let selectedMode: GameMode;
 
 const makeFoobar = () => {
-  if (foobarContainer) {
-    foobarContainer.innerHTML = `
-        <h2>Foobar Output</h2>
-        <ul class="number-list">
-          ${foobar()
-            .map((item, index) => `<li key="${index}">${item}</li>`)
-            .join('')}
-        </ul>
-      `;
-  }
+  foobarContainer.innerHTML = `
+  <h2>Foobar Output</h2>
+  <ul class="number-list">
+    ${foobar()
+      .map((item, index) => `<li key="${index}">${item}</li>`)
+      .join('')}
+  </ul>
+`;
 };
 
-let isFoobarOpen = foobarContainer && foobarContainer.style.display === 'block';
-let isPlayOpen = gameWrapper && gameWrapper.style.display === 'block';
+let isFoobarOpen = foobarContainer.style.display === 'block';
+let isPlayOpen = gameWrapper.style.display === 'block';
 
 const toggleMenuContainers = (id: string | undefined) => {
   if (id === '1') {
     if (!isFoobarOpen && !isPlayOpen) {
-      if (foobarContainer) foobarContainer.style.display = 'none';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'View foobar';
-      if (gameWrapper) gameWrapper.style.display = 'none';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Play Rock-Paper-Scissors game';
+      foobarContainer.style.display = 'none';
+      toggleFoobarButton.textContent = 'View foobar';
+      gameWrapper.style.display = 'none';
+      toggleChooseModeButton.textContent = 'Play Rock-Paper-Scissors game';
     }
     if (isFoobarOpen && isPlayOpen) {
-      if (foobarContainer) foobarContainer.style.display = 'block';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'View foobar';
-      if (gameWrapper) gameWrapper.style.display = 'block';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Hide game';
+      foobarContainer.style.display = 'block';
+      toggleFoobarButton.textContent = 'View foobar';
+      gameWrapper.style.display = 'block';
+      toggleChooseModeButton.textContent = 'Hide game';
       isPlayOpen = !isPlayOpen;
     }
     if (isFoobarOpen && !isPlayOpen) {
       makeFoobar();
-      if (foobarContainer) foobarContainer.style.display = 'block';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'Hide foobar';
-      if (gameWrapper) gameWrapper.style.display = 'none';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Play Rock-Paper-Scissors game';
+      foobarContainer.style.display = 'block';
+      toggleFoobarButton.textContent = 'Hide foobar';
+      gameWrapper.style.display = 'none';
+      toggleChooseModeButton.textContent = 'Play Rock-Paper-Scissors game';
     }
     if (!isFoobarOpen && isPlayOpen) {
-      if (foobarContainer) foobarContainer.style.display = 'none';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'View foobar';
-      if (gameWrapper) gameWrapper.style.display = 'block';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Hide game';
+      foobarContainer.style.display = 'none';
+      toggleFoobarButton.textContent = 'View foobar';
+      gameWrapper.style.display = 'block';
+      toggleChooseModeButton.textContent = 'Hide game';
     }
   } else if (id === '2') {
     if (!isFoobarOpen && !isPlayOpen) {
-      if (gameWrapper) gameWrapper.style.display = 'none';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Play Rock-Paper-Scissors game';
-      if (foobarContainer) foobarContainer.style.display = 'none';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'View foobar';
+      gameWrapper.style.display = 'none';
+      toggleChooseModeButton.textContent = 'Play Rock-Paper-Scissors game';
+      foobarContainer.style.display = 'none';
+      toggleFoobarButton.textContent = 'View foobar';
     }
 
     if (isFoobarOpen && isPlayOpen) {
-      if (foobarContainer) foobarContainer.style.display = 'none';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'View foobar';
-      if (gameWrapper) gameWrapper.style.display = 'block';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Hide game';
+      foobarContainer.style.display = 'none';
+      toggleFoobarButton.textContent = 'View foobar';
+      gameWrapper.style.display = 'block';
+      toggleChooseModeButton.textContent = 'Hide game';
       isFoobarOpen = !isFoobarOpen;
     }
     if (isFoobarOpen && !isPlayOpen) {
-      if (foobarContainer) foobarContainer.style.display = 'none';
-      if (toggleFoobarButton) toggleFoobarButton.textContent = 'View foobar';
-      if (gameWrapper) gameWrapper.style.display = 'block';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Hide game';
+      foobarContainer.style.display = 'none';
+      toggleFoobarButton.textContent = 'View foobar';
+      gameWrapper.style.display = 'block';
+      toggleChooseModeButton.textContent = 'Hide game';
     }
     if (!isFoobarOpen && isPlayOpen) {
-      if (gameWrapper) gameWrapper.style.display = 'block';
-      if (toggleChooseModeButton)
-        toggleChooseModeButton.textContent = 'Hide game';
+      gameWrapper.style.display = 'block';
+      toggleChooseModeButton.textContent = 'Hide game';
     }
   } else {
     throw new Error('Invalid id');
   }
 };
 
-if (toggleFoobarButton) {
+{
   toggleFoobarButton.onclick = (event) => {
     if (event.target instanceof HTMLButtonElement) {
       const id: string | undefined = event.target.dataset.id;
@@ -110,38 +123,42 @@ if (toggleFoobarButton) {
   };
 }
 
-if (toggleChooseModeButton) {
-  toggleChooseModeButton.onclick = (event) => {
-    if (event.target instanceof HTMLButtonElement) {
-      const id: string | undefined = event.target.dataset.id;
-      isPlayOpen = !isPlayOpen;
-      toggleMenuContainers(id);
-    }
-  };
-}
+toggleChooseModeButton.onclick = (event) => {
+  console.log('toggleChooseModeButton clicked');
+  if (event.target instanceof HTMLButtonElement) {
+    const id: string | undefined = event.target.dataset.id;
+    isPlayOpen = !isPlayOpen;
+    toggleMenuContainers(id);
+  }
+};
 
 const openGameDeck = () => {
-  if (gameDeck && gameModeContainer) {
-    gameDeck.style.display = 'block';
-    gameModeContainer.style.display = 'none';
-  }
+  gameDeck.style.display = 'block';
+  gameModeContainer.style.display = 'none';
 };
 
 const selectMode = (event: MouseEvent) => {
   if (event.target instanceof HTMLButtonElement) {
-    const selectedMode = event.target.dataset.mode;
+    selectedMode = event.target.dataset.mode as GameMode;
     setSelectedModeTitle(selectedMode);
     setPlayersNames(selectedMode);
     openGameDeck();
     makeDeck(selectedMode);
   }
+  if (selectedMode === 'CvsC') {
+    setTimeout(() => {
+      const {player1, player2, result} = playGame();
+      player1Move.textContent = player1.toUpperCase();
+      player2Move.textContent = player2.toUpperCase();
+      gameResultElement.textContent = result;
+      gameResultElement.style.display = 'block';
+      computersPlaceholder.style.display = 'none';
+    }, 3000);
+  }
 };
 
-modeButtons.forEach((button) => {
-  button.addEventListener('click', selectMode);
-});
-
 const setSelectedModeTitle = (selectedMode: string | undefined) => {
+  console.log('selected mode title', selectedMode);
   const titleElement = document.getElementById('selected-mode-title');
   if (titleElement && selectedMode) {
     switch (selectedMode) {
@@ -185,28 +202,7 @@ const setPlayersNames = (selectedMode: string | undefined) => {
 };
 
 const makeDeck = (selectedMode: string | undefined) => {
-  const player1Moves = document.getElementById(
-    'player1-moves',
-  ) as HTMLDivElement;
-  const player2Moves = document.getElementById(
-    'player2-moves',
-  ) as HTMLDivElement;
-  const computerPlaceholder = document.getElementById(
-    'computer-placeholder',
-  ) as HTMLDivElement;
-  const computersPlaceholder = document.getElementById(
-    'computers-placeholder',
-  ) as HTMLDivElement;
-
-  if (
-    !selectedMode ||
-    !player1Moves ||
-    !player2Moves ||
-    !computerPlaceholder ||
-    !computersPlaceholder
-  ) {
-    return;
-  }
+  if (!selectedMode) return;
 
   switch (selectedMode) {
     case 'HvsH':
@@ -226,7 +222,6 @@ const makeDeck = (selectedMode: string | undefined) => {
 };
 
 const disableButtons = (container: HTMLDivElement) => {
-  console.log('container', container);
   const buttons = container.querySelectorAll('button');
   buttons.forEach((button) => {
     button.disabled = true;
@@ -234,4 +229,80 @@ const disableButtons = (container: HTMLDivElement) => {
     button.style.color = 'darkgray';
     button.style.backgroundColor = 'lightgray';
   });
+};
+
+moveButtons.forEach((button) => {
+  button.addEventListener('click', (event: MouseEvent) => {
+    const target = event.target as HTMLButtonElement;
+    const player = parseInt(target.dataset.player || '0');
+    const move = target.dataset.move as Move;
+    handlePlayerMove(player, move);
+  });
+});
+
+modeButtons.forEach((button) => {
+  console.log('MODE button', button);
+  button.addEventListener('click', (event: MouseEvent) => {
+    selectMode(event);
+  });
+});
+
+let move1: Move | null = null;
+let move2: Move | null = null;
+
+const handlePlayerMove = (player: number, move: Move) => {
+  if (isValidMove(move)) {
+    switch (player) {
+      case 1:
+        move1 = move;
+        player1Move.textContent = move.toUpperCase();
+        disableButtons(player1Moves);
+        player1Move.style.display = 'inline';
+        break;
+      case 2:
+        move2 = move;
+        player2Move.textContent = move.toUpperCase();
+        disableButtons(player2Moves);
+        player2Move.style.display = 'inline';
+        break;
+      default:
+        break;
+    }
+
+    if (selectedMode === 'HvsH' && move1 && move2) {
+      play(move1, move2);
+    } else if (selectedMode === 'HvsC' && move1) {
+      play(move1);
+    }
+  } else {
+    return;
+  }
+};
+
+const play = (move1?: Move, move2?: Move) => {
+  let outcome: Result | null = null;
+  switch (selectedMode) {
+    case 'HvsH':
+      if (move1 && move2) {
+        const gameResult = playGame(move1, move2);
+        outcome = gameResult.result;
+        player1Move.textContent = move1.toUpperCase() || '';
+        player1Move.style.display = 'inline';
+      }
+      break;
+    case 'HvsC':
+      if (move1) {
+        const {player2, result} = playGame(move1);
+        outcome = result;
+        player2Move.textContent = player2.toUpperCase() || '';
+        player2Move.style.display = 'inline';
+        computerPlaceholder.style.display = 'none';
+      }
+      break;
+    default:
+      break;
+  }
+
+  gameResultElement.textContent = outcome;
+  gameResultElement.style.display = 'block';
 };
